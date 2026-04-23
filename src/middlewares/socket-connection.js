c4b7cbaff109e60bc54f523290d261c7fcdff9a1
Mark Joseph Salem <125126248+MarkSalem804@users.noreply.github.com>
2026-04-23 09:02:17 +0800

    Fixed socket connection when submitting requests

--- src/middlewares/socket-connection.js
+++ src/middlewares/socket-connection.js
@@ -7,10 +7,14 @@ const socket = {
     io = new Server(httpServer, {
       cors: {
         origin: [
+          "http://localhost:5173",
+          "http://localhost:5174",
+          "http://localhost:5175",
           "http://localhost:3000",
+          "http://127.0.0.1:5173",
+          "http://127.0.0.1:5174",
           "http://127.0.0.1:3000",
           "https://sdoictripticket.depedimuscity.com",
-          "https://sdotripticket.depedimuscity.com"
         ],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         credentials: true
